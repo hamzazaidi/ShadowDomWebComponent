a560664c1b5717c29386adf886d85516ff685dbd
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2019-06-15 20:25:25 -0500

    Clean up css

--- VanillaComponent/component/progressbar.js
+++ VanillaComponent/component/progressbar.js
@@ -33,22 +33,29 @@ class ProgressBar extends HTMLElement {
   connectedCallback() {
     var template = `
         <style>
-          .progress-bar {
-            width: 100%;
-            height: 30px;
-            background-color: #EDF2F4;
-            border-radius: 5px;
-            color: #FFF;
-          }
+        .progress-bar {
+          width: 100%;
+          height: 30px;
+          background-color: #edf2f4;
+          border-radius: 5px;
+          color: #fff;
+          border: 1px solid #1c3056;
+          transition: box-shadow 100ms ease-in;
+        }
 
-          .progress-bar-inner {
-            height: 100%;
-            line-height: 30px;
-            background: #2B2D42;
-            text-align: center;
-            border-radius: 5px;
-            transition: width 0.25s;
-          }
+        .progress-bar:hover {
+          box-shadow: 0px 2px 14px -5px rgba(0, 0, 0, 0.75);
+        }
+
+        .progress-bar-inner {
+          height: 100%;
+          line-height: 30px;
+          background: #363e8e;
+          text-align: center;
+          border-radius: 5px;
+          transition: width 0.25s;
+        }
+
         </style>
         <div class="progress-bar">
           <div class="progress-bar-inner">${this.complete}%</div>
